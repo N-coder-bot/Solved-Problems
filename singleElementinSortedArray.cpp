10df2969406a68f2f6e498025008e87f5bfdebca
The Great Pretender <77911695+N-coder-bot@users.noreply.github.com>
2022-01-30 18:58:55 +0530

    Add files via upload

---
+++ singleElementinSortedArray.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        if(nums.size()==1) return nums[0];
+        int low = 0;
+        int high = nums.size()-2;
+        
+        while(low<=high){
+            int mid = low + (high-low)/2;
+            if(nums[mid]==nums[mid^1]){
+                low = mid+1;
+            }
+            else{
+                high = mid-1;
+            }
+        }
+        return nums[low];
+    }
+};
